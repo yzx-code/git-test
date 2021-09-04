fbd11deac7680a49a4f6fe8c41e9bddd43340154
Dio Brando <yzx_123_abc@163.com>
2021-09-04 10:08:37 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author H
+ * @date 2021/9/4 - 9:21
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
